1fb37803d3abd1a34e1ff007b955f748a89e440f
3328702107 <3328702107@qq.com>
2024-04-25 20:58:52 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int nums[26];
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+	int temp_max = nums[n - 1];
+	int max_cnt = 1;
+	vector<int> dp(n+1, 1);
+	for (int i = n - 2; i >= 0; i--) {
+		if (nums[i] >= temp_max) {
+			temp_max = nums[i];
+			dp[i] = max_cnt++;
+		}
+		else {
+			if (nums[i] >= nums[i + 1]) {
+				dp[i] = dp[i + 1] + 1;
+			}
+		}
+	}
+	cout << max_cnt << endl;
+	return 0;
+}
